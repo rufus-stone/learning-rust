62742f94f0dae494d5119e1aff8f03d7e7ebbed2
rufus-stone <60827786+rufus-stone@users.noreply.github.com>
2022-10-05 09:47:58 +0100

    node example

--- egui/noddy/src/main.rs
+++ egui/noddy/src/main.rs
@@ -1,3 +1,24 @@
+#![forbid(unsafe_code)]
+#![cfg_attr(not(debug_assertions), deny(warnings))] // Forbid warnings in release builds
+#![warn(clippy::all, rust_2018_idioms)]
+
+// When compiling natively:
+#[cfg(not(target_arch = "wasm32"))]
 fn main() {
-    println!("Hello, world!");
+    use eframe::egui::Visuals;
+    use noddy::NodeGraphExample;
+
+    eframe::run_native(
+        "Egui node graph example",
+        eframe::NativeOptions::default(),
+        Box::new(|cc| {
+            cc.egui_ctx.set_visuals(Visuals::dark());
+            #[cfg(feature = "persistence")]
+            {
+                Box::new(app::NodeGraphExample::new(cc))
+            }
+            #[cfg(not(feature = "persistence"))]
+            Box::new(noddy::NodeGraphExample::default())
+        }),
+    );
 }
